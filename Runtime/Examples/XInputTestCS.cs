bfbd0a6d418b263c90b370caa1422e9d8242c32b
Eloi Stree <eloistree@gmail.com>
2020-11-26 15:11:14 +0100

    Save: 11/26/2020 3:11 PM

--- Runtime/Examples/XInputTestCS.cs
+++ Runtime/Examples/XInputTestCS.cs
@@ -24,39 +24,6 @@ public class XInputTestCS : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Find a PlayerIndex, for a single player game
-        // Will find the first controller that is connected ans use it
-        if (!playerIndexSet || !prevState.IsConnected)
-        {
-            for (int i = 0; i < 4; ++i)
-            {
-                PlayerIndex testPlayerIndex = (PlayerIndex)i;
-                GamePadState testState = GamePad.GetState(testPlayerIndex);
-                if (testState.IsConnected)
-                {
-                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
-                    playerIndex = testPlayerIndex;
-                    playerIndexSet = true;
-                }
-            }
-        }
-
-        prevState = state;
-        state = GamePad.GetState(playerIndex);
-
-        // Detect if a button was pressed this frame
-        if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
-        {
-            GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
-        }
-        // Detect if a button was released this frame
-        if (prevState.Buttons.A == ButtonState.Pressed && state.Buttons.A == ButtonState.Released)
-        {
-            GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-        }
-
-        // Make the current object turn
-        transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Left.X * 25.0f * Time.deltaTime, 0.0f);
     }
 
     void OnGUI()
